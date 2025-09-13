30d636a1d3df23917ecb272635051af377809dd7
asfsdw <solra@daum.net>
2025-09-13 23:08:57 +0900

    20250913_2308

--- src/main/java/com/spring/springGroupS/controller/AdminController.java
+++ src/main/java/com/spring/springGroupS/controller/AdminController.java
@@ -38,6 +38,7 @@ public class AdminController {
 		// adminMain에서 건 수 보여주기 위해 newCount만 뽑아서 보낸다.
 		int guestNewCount = gVOS.get(0).getNewCount();
 		int memberNewCount = mVOS.get(0).getNewCount();
+		// 탈퇴대기중인 회원 건 수.
 		int cancelMember = mVOS.get(0).getCancelMember();
 		
 		model.addAttribute("guestNewCount", guestNewCount);
@@ -54,6 +55,41 @@ public class AdminController {
 	public String adminContentGet() {
 		return "admin/adminContent";
 	}
+	
+	//관리자 메뉴에서 방명록 리스트 보여주기.
+	@GetMapping("/guest/GuestList")
+	public String guestListGet(Model model, HttpSession session,
+			String flag,
+			@RequestParam(name="pag", defaultValue = "1", required = false) int pag,
+			@RequestParam(name="pageSize", defaultValue = "10", required = false) int pageSize) {
+		// 7일 이내에 올린 방명록만 보여줄 때의 페이징처리를 위해 flag변수 사용.
+		int totRecCnt = guestService.getTotRecCnt(flag);
+		int totPage = (int)Math.ceil((double)totRecCnt/pageSize);
+		int startIndexNo = (pag-1) * pageSize;
+		int curScrStartNo = totRecCnt - startIndexNo;
+		
+		int blockSize = 3;
+		int curBlock = (pag - 1) / blockSize;
+		int lastBlock = (totPage - 1) / blockSize;
+
+		List<GuestVO> vos = null;
+		// flag가 null이거나 공백이면 전체 리스트를 vos에 담는다. flag가 null이 아니면 최근 7일의 리스트를 vos에 담는다.
+		if(flag == null || flag.equals("")) vos = guestService.getGuestList(startIndexNo, pageSize);
+		else vos = guestService.getNewGuestList(flag);
+		
+		model.addAttribute("vos", vos);
+		
+		model.addAttribute("flag", flag);
+		model.addAttribute("pag", pag);
+		model.addAttribute("pageSize", pageSize);
+		model.addAttribute("totPage", totPage);
+		model.addAttribute("curScrStartNo", curScrStartNo);
+		model.addAttribute("blockSize", blockSize);
+		model.addAttribute("curBlock", curBlock);
+		model.addAttribute("lastBlock", lastBlock);
+		
+		return "admin/guest/guestList";
+	}
 
 	// 관리자 메뉴에서 회원 리스트 보여주기.
 	@GetMapping("/member/MemberList")
@@ -62,6 +98,7 @@ public class AdminController {
 			@RequestParam(name="pag", defaultValue = "1", required = false) int pag,
 			@RequestParam(name="pageSize", defaultValue = "10", required = false) int pageSize,
 			@RequestParam(name="level", defaultValue = "100", required = false) int level) {
+		// 7일 이내에 가입한 사람만 보여줄 때의 페이징처리를 위해 flag변수 사용.
 		int totRecCnt = memberService.getTotRecCnt(flag);
 		int totPage = (int)Math.ceil((double)totRecCnt/pageSize);
 		int startIndexNo = (pag-1) * pageSize;
@@ -95,39 +132,5 @@ public class AdminController {
 		int res = memberService.setMemberLevelUp(mid, level);
 		if(res != 0) return "1";
 		else return "0";
-	}
-	
-	//관리자 메뉴에서 방명록 리스트 보여주기.
-	@GetMapping("/guest/GuestList")
-	public String guestListGet(Model model, HttpSession session,
-			String flag,
-			@RequestParam(name="pag", defaultValue = "1", required = false) int pag,
-			@RequestParam(name="pageSize", defaultValue = "10", required = false) int pageSize) {
-		int totRecCnt = guestService.getTotRecCnt(flag);
-		int totPage = (int)Math.ceil((double)totRecCnt/pageSize);
-		int startIndexNo = (pag-1) * pageSize;
-		int curScrStartNo = totRecCnt - startIndexNo;
-		
-		int blockSize = 3;
-		int curBlock = (pag - 1) / blockSize;
-		int lastBlock = (totPage - 1) / blockSize;
-
-		List<GuestVO> vos = null;
-		// flag가 null이거나 공백이면 전체 리스트를 vos에 담는다. flag가 null이 아니면 최근 7일의 리스트를 vos에 담는다.
-		if(flag == null || flag.equals("")) vos = guestService.getGuestList(startIndexNo, pageSize);
-		else vos = guestService.getNewGuestList(flag);
-		
-		model.addAttribute("vos", vos);
-		
-		model.addAttribute("flag", flag);
-		model.addAttribute("pag", pag);
-		model.addAttribute("pageSize", pageSize);
-		model.addAttribute("totPage", totPage);
-		model.addAttribute("curScrStartNo", curScrStartNo);
-		model.addAttribute("blockSize", blockSize);
-		model.addAttribute("curBlock", curBlock);
-		model.addAttribute("lastBlock", lastBlock);
-		
-		return "admin/guest/guestList";
 	}
 }
